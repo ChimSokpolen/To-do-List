526a6727f9ef8302dc1420ca509d4355ecbff396
Polen <147795380+codingb21@users.noreply.github.com>
2024-04-08 21:22:46 +0700

    First Commit
    First Date: 08-04-2024

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+const App = () => {
+  return (
+    <div>
+      <TodoList contractAddress={"0xEFbf6d78b028f289C1144f6a240fA08eDf060639"}/>
+    </div>
+  );
+};
+
+export default App;
